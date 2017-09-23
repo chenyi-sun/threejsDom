3e904b3ff365b78b9bf35c7b26b6f68a71b6c558
/ </>
2017-09-23 18:20:56 +0800

    add

--- book/canvas2017-9-23/index.js
+++ book/canvas2017-9-23/index.js
@@ -1,3 +1,11 @@
 function main(){
-    console.log('sss');
+    var canvas = document.getElementById('example');
+    if(!canvas){
+        console.log('Failed to retrleve the <canvas> element');
+        return false;
+    }
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
+    ctx.fillRect(120, 10, 150, 150);
 }
